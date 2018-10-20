4419c6ac2fc1b02f25a2b49646526f7f64d0c705
kanra <aratamagna@gmail.com>
2018-10-20 17:13:53 -0300

    del log

--- routes/router.js
+++ routes/router.js
@@ -91,7 +91,6 @@ router.post('/upload', multer(multerConfig).single('input'),function(req, res){
   },
   json: true
 }, function (err, resp, body) {
-  console.log(err)
   res.send(body);
 });
 });
